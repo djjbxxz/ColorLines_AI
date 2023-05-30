Bug Fix Now the backpropogation will be done at the right time

Myreplay_buffer.py
@@ -1,38 +1,35 @@
+from tf_agents.policies import py_tf_eager_policy
 import config
 
 import threading
 from Env import ColorLineEnv
 from agent import SacdAgent
-from tf_agents.drivers import dynamic_step_driver
-from tf_agents.replay_buffers import tf_uniform_replay_buffer
+from tf_agents.drivers import dynamic_step_driver, py_driver
+from tf_agents.replay_buffers import reverb_replay_buffer, reverb_utils
 from tf_agents.policies.random_tf_policy import RandomTFPolicy
-
-
+from tf_agents.specs import tensor_spec
+import reverb
+import tensorflow as tf
 class Replay_buffer():
 
-    def __init__(self, train_env: ColorLineEnv, agent: SacdAgent, multithread=False) -> None:
+    def __init__(self, train_env: ColorLineEnv, agent: SacdAgent, update_interval:int) -> None:
         self.train_env = train_env
         self.agent = agent
-        self.multithread = multithread
-        self.get_replay_buffer()
+        self.get_replay_buffer(update_interval)
         self._last_timestep = None
 
     def initial_fill_buffer(self,policy:RandomTFPolicy,num:int):
-        collect_driver = dynamic_step_driver.DynamicStepDriver(
+        collect_driver = py_driver.PyDriver(
             self.train_env,
-            policy,
-            num_steps=num*2,
-            observers=[self.replay_buffer.add_batch]
+            py_tf_eager_policy.PyTFEagerPolicy(policy,use_tf_function=True),
+            max_steps=num,
+            observers=[self.rb_observer]
         )
-        collect_driver.run()
-
-    def __thread_collect_data(self, collect_driver, train_env):
-        assert self.multithread, 'No multithread mode'
-        time_step = train_env.reset()
-        while(True):
-            time_step, _ = collect_driver.run(time_step)
+        collect_driver.run(self.train_env.reset())
 
     def collect(self,num_batch):
+        if self._last_timestep == None:
+            self._last_timestep = self.train_env.reset()
         for _ in range(num_batch):
             time_step, _ = self._driver.run(time_step=self._last_timestep)
             self._last_timestep = time_step
@@ -45,24 +42,44 @@ class Replay_buffer():
     def collect_driver(self) -> dynamic_step_driver.DynamicStepDriver:
         return self._driver
 
-    def get_replay_buffer(self):
+    def get_replay_buffer(self,update_interval:int):
         agent = self.agent
-        replay_buffer = tf_uniform_replay_buffer.TFUniformReplayBuffer(
-            agent.collect_data_spec,
-            batch_size=config.gradient_step,
-            max_length=config.replay_buffer_max_length)
 
-        collect_driver = dynamic_step_driver.DynamicStepDriver(
+        table_name = 'table'
+        replay_buffer_signature = tensor_spec.from_spec(
+            agent.collect_data_spec)
+        replay_buffer_signature = tensor_spec.add_outer_dim(
+            replay_buffer_signature)
+
+        table = reverb.Table(
+            table_name,
+            max_size=config.replay_buffer_max_length,
+            sampler=reverb.selectors.Prioritized(0.6),
+            remover=reverb.selectors.Fifo(),
+            rate_limiter=reverb.rate_limiters.MinSize(1),
+            signature=replay_buffer_signature)
+
+        reverb_server = reverb.Server([table])
+
+        replay_buffer = reverb_replay_buffer.ReverbReplayBuffer(
+            agent.collect_data_spec,
+            table_name=table_name,
+            sequence_length=2,
+            local_server=reverb_server)
+
+        self.rb_observer = reverb_utils.ReverbAddTrajectoryObserver(
+            replay_buffer.py_client,
+            table_name,
+            sequence_length=2)
+
+        
+        collect_driver = py_driver.PyDriver(
             self.train_env,
-            agent.collect_policy,
-            num_steps=2,
-            observers=[replay_buffer.add_batch]
+            py_tf_eager_policy.PyTFEagerPolicy(agent.collect_policy,use_tf_function=True),
+            observers=[self.rb_observer],
+            max_steps=update_interval
         )
 
-        if self.multithread:
-            self.__worker_thread = threading.Thread(target=self.__thread_collect_data,
-                                                    args=(collect_driver, self.train_env))
-            self.__worker_thread.setDaemon(True)
         dataset = replay_buffer.as_dataset(
             num_parallel_calls=3,
             sample_batch_size=config.batch_size,
@@ -71,5 +88,9 @@ class Replay_buffer():
         self._driver = collect_driver
         self.replay_buffer = replay_buffer
 
-    def start_collect_data(self):
-        self.__worker_thread.start()
+    def update_priorities(self,keys,priorities):
+        #return np.clip((p + self.eps) ** self.alpha, self.min_pa, self.max_pa)
+        priorities = (priorities+0.01)**0.6
+        priorities = tf.clip_by_value(priorities,0,1)
+        priorities = tf.cast(priorities,tf.float64)
+        self.replay_buffer.update_priorities(keys,priorities)

agent.py
@@ -53,7 +53,7 @@ from tf_agents.eval import metric_utils
 import tensorflow_probability as tfp
 import train_config
 SacLossInfo = collections.namedtuple(
-    'SacLossInfo', ('critic_loss', 'actor_loss', 'alpha_loss'))
+    'SacLossInfo', ('critic_loss', 'actor_loss', 'alpha_loss','error'))
 
 
 class Checkpoint:
@@ -324,7 +324,7 @@ class SacdAgent(tf_agent.TFAgent):
         with tf.GradientTape(watch_accessed_variables=False, persistent=True) as tape:
             tape.watch(trainable_critic1_variables)
             tape.watch(trainable_critic2_variables)
-            critic_loss1, critic_loss2 = self.critic_loss(
+            critic_loss1, critic_loss2, error, q1, q2 = self.critic_loss(
                 time_steps,
                 actions,
                 next_time_steps,
@@ -344,10 +344,6 @@ class SacdAgent(tf_agent.TFAgent):
             critic_loss1, trainable_critic1_variables)
         critic2_grads = tape.gradient(
             critic_loss2, trainable_critic2_variables)
-        self._apply_gradients(critic1_grads, trainable_critic1_variables,
-                              self._critic_optimizer1)
-        self._apply_gradients(critic2_grads, trainable_critic2_variables,
-                              self._critic_optimizer2)
 
         trainable_actor_variables = self._actor_network.trainable_variables
         with tf.GradientTape(watch_accessed_variables=False) as tape:
@@ -359,8 +355,6 @@ class SacdAgent(tf_agent.TFAgent):
             actor_loss *= self._actor_loss_weight
         tf.debugging.check_numerics(actor_loss, 'Actor loss is inf or nan.')
         actor_grads = tape.gradient(actor_loss, trainable_actor_variables)
-        self._apply_gradients(actor_grads, trainable_actor_variables,
-                              self._actor_optimizer)
 
         alpha_variable = [self._log_alpha]
         with tf.GradientTape(watch_accessed_variables=False) as tape:
@@ -370,11 +364,14 @@ class SacdAgent(tf_agent.TFAgent):
             alpha_loss *= self._alpha_loss_weight
         tf.debugging.check_numerics(alpha_loss, 'Alpha loss is inf or nan.')
         alpha_grads = tape.gradient(alpha_loss, alpha_variable)
-        self._apply_gradients(alpha_grads, alpha_variable,
-                              self._alpha_optimizer)
+
+        self._apply_gradients(critic1_grads, trainable_critic1_variables,self._critic_optimizer1)
+        self._apply_gradients(critic2_grads, trainable_critic2_variables,self._critic_optimizer2)
+        self._apply_gradients(actor_grads, trainable_actor_variables,self._actor_optimizer)
+        self._apply_gradients(alpha_grads, alpha_variable,self._alpha_optimizer)
 
         self._print_log(critic_loss1, critic_loss2,
-                        actor_loss, alpha_loss, entropies)
+                        actor_loss, alpha_loss, entropies,q1,q2)
 
         self.train_step_counter.assign_add(1)
         self._update_target()
@@ -384,7 +381,7 @@ class SacdAgent(tf_agent.TFAgent):
         total_loss = critic_loss + actor_loss + alpha_loss
 
         extra = SacLossInfo(
-            critic_loss=critic_loss, actor_loss=actor_loss, alpha_loss=alpha_loss)
+            critic_loss=critic_loss, actor_loss=actor_loss, alpha_loss=alpha_loss,error=error)
 
         return tf_agent.LossInfo(loss=total_loss, extra=extra)
 
@@ -481,8 +478,7 @@ class SacdAgent(tf_agent.TFAgent):
         with tf.name_scope('critic_loss'):
             nest_utils.assert_same_structure(actions, self.action_spec)
             nest_utils.assert_same_structure(time_steps, self.time_step_spec)
-            nest_utils.assert_same_structure(
-                next_time_steps, self.time_step_spec)
+            nest_utils.assert_same_structure(next_time_steps, self.time_step_spec)
 
             # Calc current Q
             pred_input = (time_steps.observation)
@@ -510,15 +506,14 @@ class SacdAgent(tf_agent.TFAgent):
             # Seperate finished states from unfinished ones
             target_q = (
                 reward_scale_factor * next_time_steps.reward +
-                (1-tf.cast(next_time_steps.is_last(), tf.float32)) * gamma * next_time_steps.discount * next_q)
-
+                (1-tf.cast(next_time_steps.is_last(), tf.float32)) * gamma * next_q)
             critic_loss1 = td_errors_loss_fn(target_q, pred_td_targets1)
             critic_loss2 = td_errors_loss_fn(target_q, pred_td_targets2)
             # critic_loss = critic_loss1 + critic_loss2
-
+            errors = tf.abs(pred_td_targets1 - target_q)
             # Sum over the time dimension.
-            critic_loss1 = tf.reduce_mean(critic_loss1)
-            critic_loss2 = tf.reduce_mean(critic_loss2)
+            critic_loss1 = tf.reduce_mean(critic_loss1*weights)
+            critic_loss2 = tf.reduce_mean(critic_loss2*weights)
 
             # agg_loss = common.aggregate_losses(
             #     per_example_loss=critic_loss,
@@ -530,7 +525,7 @@ class SacdAgent(tf_agent.TFAgent):
             self._critic_loss_debug_summaries(target_q, pred_td_targets1,
                                               pred_td_targets2)
 
-            return critic_loss1, critic_loss2
+            return critic_loss1, critic_loss2, errors, pred_td_targets1,pred_td_targets2
 
     def actor_loss(self,
                    time_steps: ts.TimeStep,
@@ -667,7 +662,7 @@ class SacdAgent(tf_agent.TFAgent):
             tf.summary.scalar(
                 name='log_alpha', data=self._log_alpha, step=self.train_step_counter)
 
-    def _print_log(self, critic1_loss, critic2_loss, actor_loss, alpha_loss, entropies):
+    def _print_log(self, critic1_loss, critic2_loss, actor_loss, alpha_loss, entropies,q1,q2):
         if self.train_step_counter % self._log_interval == 0:
             with tf.name_scope('Losses'):
                 tf.summary.scalar(
@@ -683,6 +678,12 @@ class SacdAgent(tf_agent.TFAgent):
                     name='entropy', data=tf.reduce_mean(entropies), step=self.train_step_counter)
                 tf.summary.scalar(
                     name='target_entropy', data=self._target_entropy, step=self.train_step_counter)
+                tf.summary.scalar(
+                    name='alpha', data=tf.exp(self._log_alpha), step=self.train_step_counter)
+                tf.summary.scalar(
+                    name='Q1', data=tf.reduce_mean(q1), step=self.train_step_counter)
+                tf.summary.scalar(
+                    name='Q2', data=tf.reduce_mean(q2), step=self.train_step_counter)
 
     def eval(self, eval_env):
         if self._train_step_counter % self._eval_interval == 0:

train.py
@@ -19,13 +19,7 @@ reduce_gpu_memory_usage()
 
 def run(args):
 
-    env = tf_py_environment.TFPyEnvironment(
-        batched_py_environment.BatchedPyEnvironment(
-            [ColorLineEnv()for _ in range(_config.gradient_step)],
-            multithreading=False
-        ),
-        isolation=True
-    )
+    env =ColorLineEnv()
     test_env = ColorLineEnv()
     test_env = tf_py_environment.TFPyEnvironment(test_env)
 
@@ -35,7 +29,7 @@ def run(args):
         name = 'shared-' + name
     time = datetime.now().strftime("%Y%m%d-%H%M")
     log_dir = os.path.join(
-        '/train_logs', f'{time}')
+        'train_logs', f'{time}')
 
     # Create the agent.
 
@@ -56,7 +50,7 @@ def run(args):
         alpha_optimizer=tf.compat.v1.train.AdamOptimizer(lr),
         save_dir=log_dir
     )
-    replay_buffer = Replay_buffer(train_env=env, agent=agent)
+    replay_buffer = Replay_buffer(train_env=env, agent=agent,update_interval=train_config.update_interval)
 
     if len(args.load) > 0:
         agent.load(
@@ -69,8 +63,9 @@ def run(args):
             agent._random_policy, train_config.init_fill)
         for i in range(train_config.num_steps):
             replay_buffer.collect(1)
-            data, _ = next(replay_buffer.iterator)
-            agent.train(data)
+            data, sample_info = next(replay_buffer.iterator)
+            loss_info = agent.train(data,weights = tf.cast(sample_info.priority[:,0],tf.float32))
+            replay_buffer.update_priorities(sample_info.key[:,0],loss_info.extra.error)
             step = agent.train_step_counter.numpy()
             print(
                 f"{step}/{train_config.num_steps}, {np.round(step/train_config.num_steps*100,2)}%")